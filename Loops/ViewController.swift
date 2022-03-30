81e9e6e97cf61ce9867086bd3931652e9c2bff3d
Luis Segovia <luiissegovia@icloud.com>
2022-03-30 14:14:27 -0600

    added loops

--- Loops/ViewController.swift
+++ Loops/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var output = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func ForLoop(_ sender: Any)
+    {
+        var veggies = ["broccoli","carrot","spinach","celery"]
+             
+        for i in 0...3
+        {
+            output += "\(veggies[i])\n"
+        }
+        print(output)
+    }
+    
+    @IBAction func joe(_ sender: Any)
+    {
+        var drinks = ["tea","coffee","milk","soda"]
+        
+        for i in 0..<drinks.count
+        {
+           output += "\(drinks[i])\n"
+        }
+        
+        
+    }
 }
 
